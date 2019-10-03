8fe8a77a2c1f92bd84a45bb94ad19dd449e7f497
laureae_sd2082@CEACCS.NET <laureae_sd2082@CEACCS.NET>
2019-10-03 13:31:39 +0800

    express practice

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 var cors = require('cors')
+var mysql = require('mysql')
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -18,5 +19,50 @@ app.post('/user', (req, res) => {
         password: null
     })
 })
+
+app.get('/db/retrieve/:Username', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mine'
+    })
+
+    connection.connect()
+
+    connection.query(`SELECT * FROM sample where Username='${req.params.Username}'`, function (err, rows, fields) {
+        if (err) throw err
+
+        res.json({
+            data: rows,
+            params: req.params,
+            Username: req.params.Username
+        })
+    })
+    connection.end()
+})
+
+app.get('/db/create/:Username/:Email/:Password', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mine'
+    })
+
+    connection.connect()
+
+    connection.query(`INSERT INTO sample(id, Username, Email, Password) VALUES ('3', '${req.params.Username}', '${req.params.Email}','${req.params.Password}')`, function (err, rows, fields) {
+        if (err) throw err
+
+        res.json({
+            data: rows,
+            params: req.params,
+            Username: req.params.Username
+        })
+    })
+    connection.end()
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
